298f951896045e673fe44fe37e5221fff1d8fa3b
EldritchSalamancer <147461070+CCroyPerrett@users.noreply.github.com>
2024-10-09 19:12:42 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("Button");
+//button.type = "button";
+button.innerHTML = "Investigate 🔎";
+app.append(button);
