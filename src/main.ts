26c8b74c2a6ca384b245af9c2913d0bce69950ce
EldritchSalamancer <147461070+CCroyPerrett@users.noreply.github.com>
2024-10-09 20:18:03 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,37 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("Button");
+const button = document.createElement("button");
 //button.type = "button";
 button.innerHTML = "Investigate 🔎";
+
 app.append(button);
+
+/*function incriment(){
+    const counter = document.getElementById("counter");
+    if(counter != null){
+        count ++; counter.innerHTML = "Clues Found = " + count;
+        console.log("count is now: " + count);
+    }
+    //count ++; counter.innerHTML = "Clues Found = " + count;
+    console.log("count is now: " + count);
+    
+  }*/
+
+let count: number = -1; count ++;
+const counter = document.createElement("div"); counter.id = "counter";
+counter.innerHTML = "Clues Found = " + count;
+app.append(counter);
+
+button.addEventListener("click", () => {
+    counter.innerHTML = "Clues Found = " + ++count;
+})
+
+//app.append(cnt);
+
+/*function handleClick(event: MouseEvent) {
+    console.log('Button clicked!');
+}
+const element = document.querySelector('.element');
+if(element!= null)
+element.addEventListener('click', handleClick);*/
